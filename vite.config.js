662d5bcbb1d3fcc5851803cf25e68f12f6c82998
Aniuzu <deadmanxkc16@gmail.com.com>
2025-06-07 23:17:18 +0100

    Remove vite-plugin-vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()], // Remove vite-plugin-vercel
   base: '/',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'] // Add other extensions if needed
+  }
 })
