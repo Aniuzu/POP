bb70f68a17b724ebf6202aa75924b1a580acb54b
Aniuzu <deadmanxkc16@gmail.com.com>
2025-06-07 23:03:32 +0100

    trd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 ﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   base: '/', // Must be '/' for Vercel
 })
