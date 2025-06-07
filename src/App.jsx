56177076c6e95fe0605ae2c68606c7f2c1f33245
Aniuzu <deadmanxkc16@gmail.com.com>
2025-06-07 23:16:11 +0100

    Remove vite-plugin-vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 import About from "./pages/About";
 import Quote from './Pages/Quote';
 import Products from "./components/Products"
