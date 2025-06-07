d1c37babc11f2335b9f7afa0687025a3adedfd34
Aniuzu <deadmanxkc16@gmail.com.com>
2025-06-07 22:58:47 +0100

    Ready for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-﻿
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Critical for Vercel
-  build: {
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name]-[hash].[ext]",
-        entryFileNames: "assets/[name]-[hash].js"
-      }
-    }
-  }
-});
-'@ | Out-File -FilePath vite.config.js -Encoding utf8 -Force'
+  base: '/', // Must be '/' for Vercel
+})
